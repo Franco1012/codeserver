df476b3fcebdce7bfce1005fbe83fb261f1fb8d4
Franco1012 <118076737+Franco1012@users.noreply.github.com>
2024-06-01 20:59:05 -0300

    sprint7 (#10)

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,27 +1,29 @@
 document.querySelector("#registerUser").addEventListener("click", async () => {
-    const data = {
-        photo: document.querySelector("#photo").value,
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value
-    }
-    console.log("soy la data", data)
-    const opts = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    }
+    try {
+        const data = {
+            photo: document.querySelector("#photo").value,
+            email: document.querySelector("#email").value,
+            password: document.querySelector("#password").value
+        }
+        console.log("soy la data", data)
+        const opts = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
 
-    let response = await fetch("/api/sessions/register", opts)
-    response = await response.json()
-    console.log(response)
-    if (response.statusCode === 201) {
-        location.replace("/pages/login.html")
+        let response = await fetch("/api/sessions/register", opts)
+        response = await response.json()
+        console.log(response)
+        if (response.statusCode === 201) {
+            location.replace("/pages/login.html")
 
+        }
+        return alert(response.message)
+
+    } catch (error) {
+        console.log(error)
     }
-    return alert(response.message)
 });
-
-
-
--- public/scripts/signout.js
+++ public/scripts/signout.js
@@ -3,16 +3,21 @@ const signout = document.querySelector("#signout")
 signout.addEventListener("click", handleSignout)
 
 async function handleSignout() {
-    const opt = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
+    try {
+        const opt = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
         }
+        let response = await fetch("/api/sessions/signout", opt)
+        response = await response.json()
+        console.log(response)
+        if (response.statusCode === 200) {
+            location.replace("/")
+        }
+    } catch (error) {
+        console.log(error)
     }
-    let response = await fetch("/api/sessions/signout", opt)
-    response = await response.json()
-    console.log(response)
-    if (response.statusCode === 200) {
-        location.replace("/")
-    }
+
 }
